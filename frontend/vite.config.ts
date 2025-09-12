2bd03269783b3abfde7f22bbfdd447587e51e262
Artem Sushko <temagic85@gmail.com>
2025-09-12 20:50:44 +0300

    add vercel config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  build: {
+    outDir: "dist",
+  },
   server: {
     host: "0.0.0.0",
     port: 3000,
