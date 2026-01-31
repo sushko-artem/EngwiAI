330713e4173f56263dc6dfdfed2ecb76b280968e
Artem Sushko <temagic85@gmail.com>
2026-01-31 14:22:09 +0300

    test: complete testing collections feature

---
+++ frontend/src/features/collections/ui/flash-cards/flash-modal/flash-cards-modal.test.tsx
@@ -0,0 +1,58 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import { ModalFlash } from "./flash-cards-modal";
+import userEvent from "@testing-library/user-event";
+
+const mockNavigate = vi.hoisted(() => vi.fn());
+
+vi.mock("react-router-dom", () => ({
+  useNavigate: () => mockNavigate,
+}));
+
+describe("ModalFlash", () => {
+  const defaultProps = {
+    collectionId: "id-123",
+    moduleName: "test",
+    moduleLength: 5,
+    unknownTerms: 2,
+    back: vi.fn(),
+    reset: vi.fn(),
+  };
+
+  const user = userEvent.setup();
+
+  it("should render correct summary", () => {
+    render(<ModalFlash {...defaultProps} />);
+
+    expect(screen.getByRole("heading")).toHaveTextContent(
+      'Модуль "test" пройден!',
+    );
+    expect(screen.getByText("Знаю: 3")).toBeInTheDocument();
+    expect(screen.getByText("Изучил: 2")).toBeInTheDocument();
+    expect(screen.getByText("Прогресс: 60%")).toBeInTheDocument();
+  });
+
+  it("should call reset when clicking reset", async () => {
+    render(<ModalFlash {...defaultProps} />);
+
+    await user.click(screen.getByText("Пройти модуль заново"));
+
+    expect(defaultProps.reset).toHaveBeenCalled();
+  });
+
+  it("should navigate to 'edit-collection' when clicking editCollection", async () => {
+    render(<ModalFlash {...defaultProps} />);
+
+    await user.click(screen.getByText("Редактировать модуль"));
+
+    expect(mockNavigate).toHaveBeenCalledWith("/edit-collection/id-123");
+  });
+
+  it("should call back when clicking back", async () => {
+    render(<ModalFlash {...defaultProps} />);
+
+    await user.click(screen.getByText("Выбрать другой модуль"));
+
+    expect(defaultProps.back).toHaveBeenCalled();
+  });
+});
