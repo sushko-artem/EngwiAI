f0db264808f5951f5dc2c1225813eecda3643162
Artem Sushko <temagic85@gmail.com>
2026-02-03 19:04:19 +0300

    ref: add useReducer to useFlashCards

--- frontend/src/pages/FlashCards/index.tsx
+++ frontend/src/pages/FlashCards/index.tsx
@@ -1,20 +1,8 @@
 import { useParams } from "react-router-dom";
 import { FlashCardsContainer } from "@features/collections";
-import { useState } from "react";
 
 export const FlashCardsPage = () => {
   const { collectionId = "" } = useParams<{ collectionId?: string }>();
-  const [resetKey, setResetKey] = useState(0);
 
-  const resetModule = () => {
-    setResetKey((prev) => prev + 1);
-  };
-
-  return (
-    <FlashCardsContainer
-      key={resetKey}
-      collectionId={collectionId}
-      reset={resetModule}
-    />
-  );
+  return <FlashCardsContainer collectionId={collectionId} />;
 };
