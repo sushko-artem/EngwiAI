3be87a0eba69fa46a3e47d0f9f05e5c1056a1fef
Artem Sushko <temagic85@gmail.com>
2026-04-09 18:11:54 +0300

    ref: change App router, use createBrouserRouter

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,18 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { ModalRootContainer } from "@widgets/modal";
 
+const router = createBrowserRouter([
+  ...routes.map((route) => ({
+    path: route.path,
+    element: route.element,
+  })),
+]);
+
 export const App = () => (
   <Provider store={store}>
-    <Router>
-      <Routes>
-        {routes.map((route, index) => (
-          <Route key={index} path={route.path} element={route.element} />
-        ))}
-      </Routes>
-      <ModalRootContainer />
-    </Router>
+    <RouterProvider router={router} />
+    <ModalRootContainer />
   </Provider>
 );
