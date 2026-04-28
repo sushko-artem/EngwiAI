92ae162cbd7e31c19508fca7f60dd2a9bfcd92c0
Artem Sushko <temagic85@gmail.com>
2026-04-28 14:02:38 +0300

    ref: add additional prop in howler obj

--- frontend/src/shared/hooks/useSound.ts
+++ frontend/src/shared/hooks/useSound.ts
@@ -2,8 +2,16 @@ import { Howl } from "howler";
 import { useCallback } from "react";
 
 const sounds = {
-  correct: new Howl({ src: ["/sounds/correct_sound.mp3"] }),
-  incorrect: new Howl({ src: ["/sounds/incorrect_sound.mp3"] }),
+  correct: new Howl({
+    src: ["/sounds/correct_sound.mp3"],
+    volume: 0.4,
+    preload: true,
+  }),
+  incorrect: new Howl({
+    src: ["/sounds/incorrect_sound.mp3"],
+    volume: 0.4,
+    preload: true,
+  }),
 };
 
 type PlaySoundType = keyof typeof sounds;
