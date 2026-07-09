c1312f4ad2221a03a25d3e3c67b183eccffa72a0
Artem Sushko <temagic85@gmail.com>
2026-07-09 15:06:34 +0300

    ref: add Link into GenerationError component

--- frontend/src/features/grammar-test/ui/generation-error.tsx
+++ frontend/src/features/grammar-test/ui/generation-error.tsx
@@ -1,11 +1,18 @@
 import { getErrorMessage } from "@shared/api";
+import { Link } from "react-router-dom";
 
 export const GenerationError = ({ error }: { error: unknown }) => (
   <div
-    data-testid="no-collection-error"
+    data-testid="generation-error"
     className="text-3xl font-jost text-fuchsia-800 flex flex-col h-[50vh] justify-center text-center"
   >
-    <h2>Ошибка генерации</h2>
+    <h2>Ошибка генерации! Попробуйте позже.</h2>
     <p className="text-red-500 text-lg">Error: {getErrorMessage(error)}</p>
+    <Link
+      className="w-fit mx-auto mt-4 underline text-gray-700 font-jost hover:scale-[1.1]"
+      to={"/dashboard"}
+    >
+      На главную
+    </Link>
   </div>
 );
