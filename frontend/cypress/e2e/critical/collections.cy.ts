fd8fa661ca5635bd9f076dc9bc8ef682bd76abdb
Artem Sushko <temagic85@gmail.com>
2026-09-18 16:41:14 +0300

    config: add optimizeDeps.include into vite.config

--- frontend/cypress/e2e/critical/collections.cy.ts
+++ frontend/cypress/e2e/critical/collections.cy.ts
@@ -10,9 +10,7 @@ describe("Collections Page - critical", () => {
     cy.createCollection("food").then((collection) => {
       collectionsIds.push(collection.id);
     });
-    cy.intercept("GET", "/api/collections/list").as("getCollections");
     cy.visit("/collections");
-    cy.wait("@getCollections");
   });
 
   afterEach(() => {
@@ -20,6 +18,7 @@ describe("Collections Page - critical", () => {
   });
 
   it("should display list of user collections", () => {
+    cy.contains("Мои модули", { timeout: 10000 }).should("be.visible");
     cy.fixture("collections/animals").then((animals) => {
       cy.contains(`${animals.name}`).should("be.visible");
     });
